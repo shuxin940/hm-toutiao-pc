38c8bb5a5964d66bedcb3ea3fde0c0cf0fb43395
fanshuxin <2667361650@qq.com>
2019-10-26 11:43:51 +0800

    登录完成

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app">
-    hm-toutiao-pc <el-button type="success">成功按钮</el-button>
+    <!-- 一级路由的容器 -->
+    <router-view />
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
-
 }
 </script>
 
